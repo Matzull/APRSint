09b09e66d181b25ad5a909d0bdd244769f210319
Marcos <marcal16@ucm.es>
2023-11-08 00:33:46 +0100

    Fix formatting and whitespace issues in main.py and aprs_client.py

--- app/src/utils/buffer.py
+++ app/src/utils/buffer.py
@@ -2,8 +2,9 @@ from tqdm import tqdm
 import os
 import gzip
 
+
 class Buffer:
-    def __init__(self, buffer_length = 100, max_buffers_per_file=25, debug=False):
+    def __init__(self, buffer_length=100, max_buffers_per_file=25, debug=False):
         self.data = []
         self.buffer_length = buffer_length
         self.buffers_in_file = 0
@@ -25,7 +26,9 @@ class Buffer:
     def write_to_file(self, data):
         if self.buffers_in_file > self.max_buffers_per_file:
             with open(f"./database/data_{self.iterations}.txt", "r") as f:
-                with gzip.open(f"./database/data_{self.iterations}.txt.gz", "wt") as zip_file:
+                with gzip.open(
+                    f"./database/data_{self.iterations}.txt.gz", "wt"
+                ) as zip_file:
                     for line in f:
                         zip_file.write(line)
             os.remove(f"./database/data_{self.iterations}.txt")
@@ -37,7 +40,7 @@ class Buffer:
         self._flush()
         if self.debug:
             self.progress_bar.reset()
-            print('\033[4A\033[2K', end='')
-        
+            print("\033[4A\033[2K", end="")
+
     def _flush(self):
         self.data = []
