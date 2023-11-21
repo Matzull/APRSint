73bea2019a65fc8e26993acb3f68cf7b66171888
Marcos <marcal16@ucm.es>
2023-11-21 18:05:43 +0100

    Add chart and theme change functionality

--- app/src/web/dashboard/dashboard.js
+++ app/src/web/dashboard/dashboard.js
@@ -46,4 +46,47 @@
       }
     }
   })
+  const ctx1 = document.getElementById('myChart1')
+  // eslint-disable-next-line no-unused-vars
+  const myChart1 = new Chart(ctx1, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+        tooltip: {
+          boxPadding: 3
+        }
+      }
+    }
+  })
+
 })()
