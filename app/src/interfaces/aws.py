6df668e272790a79d17f3e8e6980f9e299769c99
Matzul <marcal16@ucm.es>
2024-02-05 22:43:35 +0100

    Improve AWS interface

--- app/src/interfaces/aws.py
+++ app/src/interfaces/aws.py
@@ -1,4 +1,4 @@
-import io
+from tqdm import tqdm
 import logging
 import boto3
 import configparser as cp
@@ -19,24 +19,25 @@ class S3_Storage:
             aws_access_key_id=creds["AWS_ACCESS_KEY_ID"],
             aws_secret_access_key=creds["AWS_SECRET_ACCESS_KEY"],
         )
-        self.s3 = self.session.resource("s3")
+        self.s3 = self.session.client("s3")
 
     def list_buckets(self):
-        return [bucket.name for bucket in self.s3.buckets.all()]
+        return [bucket["Name"] for bucket in self.s3.list_buckets()["Buckets"]]
         
     def upload_file(self, bucket, local_file, dest_file=None):
         if dest_file is None:
             dest_file = local_file.split("/")[-1]
-        print(os.listdir())
-        with open(local_file, "rb") as data:
-            self.s3.Bucket(bucket).put_object(Key=dest_file, Body=data) 
+        with tqdm(total= os.path.getsize(local_file),  desc=f'Destination: s3://{bucket}/{dest_file}', bar_format="{percentage:.1f}%|{bar:25} | {rate_fmt} | {desc}",  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+            self.s3.upload_file(local_file, bucket, dest_file, Callback=pbar.update)
 
     def download_file(self, bucket, bucket_file, dest_file=None):
         if dest_file is None:
-            dest_name = bucket_file
+            dest_file = bucket_file
         try:
-            response = self.s3.Object(bucket, bucket_file).get()
-            with open(dest_file, "wb") as dest_name:
-                dest_name.write(response["Body"].read())
+            meta_data = self.s3.head_object(Bucket=bucket, Key=bucket_file)
+            total_length = int(meta_data.get('ContentLength', 0))
+            with tqdm(total=total_length,  desc=f'Source: s3://{bucket}/{bucket_file}', bar_format="{percentage:.1f}%|{bar:25} | {rate_fmt} | {desc}",  unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+                self.s3.download_file(bucket, bucket_file, dest_file, Callback=pbar.update)
         except Exception as e:
+            print(f"Error: {e}")
             logger.info(f"{bucket_file} does not exist")
